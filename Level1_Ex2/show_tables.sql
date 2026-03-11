1cda3b638cc07e3ce87384b007add3bae56b9902
mar <marpouradressa@gmail.com>
2026-03-11 19:33:30 +0100

    Create show_tables.sql

---
+++ Level1_Ex2/show_tables.sql
@@ -0,0 +1,13 @@
+SELECT * FROM categories;
+
+SELECT * FROM clients;
+
+SELECT * FROM employees;
+
+SELECT * FROM order_items;
+
+SELECT * FROM orders;
+
+SELECT * FROM products;
+
+SELECT * FROM stores;
